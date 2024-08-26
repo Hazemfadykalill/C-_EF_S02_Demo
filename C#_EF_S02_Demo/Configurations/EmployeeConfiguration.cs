757a1b59f242c332e91f729ab6f1cfaf1cb0f870
Hazem Fady <hazemfady123@gmail.com>
2024-08-26 15:50:37 +0300

    Demo Revision

---
+++ C#_EF_S02_Demo/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using C__EF_S02_Demo.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__EF_S02_Demo.Configurations
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            // Define the primary key
+            builder.HasKey(e => e.Id);
+          
+
+            // Define properties
+            builder.Property(e => e.Id)
+                   .UseIdentityColumn(10,10); // Auto-incrementing ID
+
+            builder.Property(e => e.Name)
+                .HasColumnType("varchar")
+                   .IsRequired() // Make Name required
+                   .HasMaxLength(100); // Set a maximum length for Name
+
+            builder.Property(e => e.Address)
+                   .HasMaxLength(200); // Set a maximum length for Address
+
+            builder.Property(e => e.Salary)
+                   .HasColumnType("decimal(18,2)"); // Set the Salary to be a decimal with precision
+
+            builder.Property(e => e.Age)
+                   .IsRequired();
+                   
+        }
+    }
+}
---
+++ C#_EF_S02_Demo/Migrations/20240826124048_Initial Created.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace C__EF_S02_Demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "10, 10"),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
